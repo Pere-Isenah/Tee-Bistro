a21f65bc5935c86886aebd3cc9f463baa94ce5cc
Pere Isenah <pereisenah@gmail.com>
2023-09-11 20:08:26 +0100

    update assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
+'use strict';
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+/**
+ * NAVBAR
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const sliderItems = document.querySelectorAll(".slider-item");
   let currentIndex = 0;
