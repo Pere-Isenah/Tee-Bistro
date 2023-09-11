39c2e070dedffe4324310c22fbe8098756b943de
Pere Isenah <pereisenah@gmail.com>
2023-09-11 20:09:54 +0100

    update assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,8 +26,40 @@ const toggleNavbar = function () {
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
+/**
+ * HEADER & BACK TOP BTN
+ */
 
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * HERO SECTION 
+ */
 document.addEventListener("DOMContentLoaded", function () {
   const sliderItems = document.querySelectorAll(".slider-item");
   let currentIndex = 0;
